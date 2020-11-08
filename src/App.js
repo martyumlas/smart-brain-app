bf6085f1cf44fb884776262fe33aecdb2d78d346
Marty Umlas <martyumlas@gmail.com>
2020-11-08 16:14:11 +0800

    image recognition api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/rank/Rank'
 import Particles from 'react-particles-js'
 import './App.css'
+import Clarifai from 'clarifai'
+import FaceRecognition from './components/facerecognition/FaceRecognition'
+
+const app = new Clarifai.App({
+  apiKey: 'de319c321b4a441ea089c8fee08832d4'
+ });
 
 const particlesOptions = {
   particles:{
@@ -18,16 +24,57 @@ const particlesOptions = {
   }
 }
 
+
 export class App extends Component {
+   state = {
+    input: '',
+    imageUrl: '',
+    boxes: []
+  }
+
+  onImageChange = (e) => {
+    const reader = new FileReader()
+
+    reader.onload = (e) => {
+      this.setState({imageUrl: e.target.result})
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+
+
+
+  onInputChange = (e) => {
+    this.setState({input: e.target.value})
+
+  }
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => {
+        // console.log(response.outputs[0].data)
+        this.setState({boxes: response.outputs[0].data})
+
+     
+      })
+      .catch(error => {
+        // There was an error
+      });
+  }
   render() {
     return (
       <div className='App'>
-         <Particles className='particles' params={particlesOptions} />
+        <Particles className='particles' params={particlesOptions} />
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
-        {/* <FaceRecognition/> */}
+        <input type="file" name="" id="" onChange={this.onImageChange}/>
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+
+
+
       </div>
     )
   }
